78a754aab8d517500e288736b996c52e3187940c
saikirun7 <saikirunrayudu@gmail.com>
2025-12-17 14:46:57 +0530

    s

--- task.sh
+++ task.sh
@@ -1,25 +1,11 @@
-#Task 1: Hello User
-echo "Please enter your name to print"
-read name
-echo "Hello, $name"
+ID=$(id -u)
 
-#Task 2: Age Checker
-echo "Enter Age"
-read age
-if [ $age -ge 13 ] && [ $age -le 19 ]; then 
-echo "He is teenager"
-elif [ $age -ge 20 ]; then
-echo "He is adult"
+if [ $ID -ne 0 ]
+then
+   echo "ERROR:: Please run the script with root access"
+   exit 1
 else
-echo "He is child"
+   echo "You are a root user"
 fi
 
-#Task 3: Directory Creator
-echo "Enter Folder Name"
-read name
-if [ -d $name ]; then
-echo "Folder already exists"
-else 
-mkdir "$name"
-echo "Folder created"
-fi
+echo "Completed"
